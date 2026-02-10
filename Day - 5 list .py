16f81b506a7400e6bb708c996c7d0765489a561e
P.GOVINDU <vnani8219@gmail.com>
2026-02-10 19:45:21 +0530

    Add files via upload

---
+++ Day - 5 list .py
@@ -0,0 +1,29 @@
+#List in python 
+
+l=[70,'vijay',20.4, [2,4]]
+print(l)
+
+vijay=[1,2.2,'python']
+vijay1=list([3,2.2])
+print(vijay)
+print(vijay1)
+
+
+snadeep=[1.2,2,3,4,5.9]
+print(snadeep)
+
+
+James_bond=[7.2,28,2.3,True]
+print(James_bond)
+
+
+#replacce name
+james_bond=[1.2,12,34,32,'vijay']
+james_bond[0]="vijay"
+print(james_bond)
+
+'''
+Sliceing in list (start,stop,skip)
+'''
+vijay=[1,2,3,4,5,6,7,8,90,10]
+print (vijay[2:8:2])
